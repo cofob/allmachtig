fa4ede97f9ae91a2e67512d9ad31ba3523793196
cofob <cofob@tutanota.com>
2021-03-24 16:40:39 +0700

    Добавлены таблицы

--- models.py
+++ models.py
@@ -8,5 +8,22 @@ class Base(Model):
         database = db
 
 
-db.create_tables([])
+class User(Base):
+    user_id = IntegerField(unique=True)
+
+
+class Guild(Base):
+    guild_id = IntegerField(unique=True)
+    settings = BlobField(null=True)
+
+
+class UserInGuild(Base):
+    user = ForeignKeyField(User)
+    guild = ForeignKeyField(User)
+    settings = BlobField(null=True)
+    roles = BlobField(null=True)
+    guild_username = BlobField(null=True)
+
+
+db.create_tables([User, Guild, UserInGuild])
 db.connect()
