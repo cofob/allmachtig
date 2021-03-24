79874f61ccc983d4af14d535ffa2c09e3382e19f
cofob <cofob@tutanota.com>
2021-03-24 19:07:02 +0700

    Добавлено оповещение админа в телеграм о запуске

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import discord
 import os
 from discord_slash import SlashCommand
 
+# отправляем сообщение админу о загрузке
+try:
+    os.system('python3 backup_db.py boot')
+except:
+    pass
+
 bot = commands.Bot(command_prefix=os.environ.get('PREFIX', '.'), intents=discord.Intents.all(),
                    help_command=None)
 slash = SlashCommand(bot, sync_commands=True)
