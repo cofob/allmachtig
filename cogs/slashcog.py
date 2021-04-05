030a9023d3f1755c5c5d65089c0328a5f727d795
cofob <cofob@tutanota.com>
2021-04-06 01:57:47 +0200

    Revert "Merge dev to master"

--- cogs/slashcog.py
+++ cogs/slashcog.py
@@ -16,22 +16,7 @@ class SlashCog(commands.Cog):
     @cog_ext.cog_slash(name='ping', description="Get bot latency.")
     async def ping(self, ctx: SlashContext):
         await ctx.respond()
-        await ctx.send(embed=await self.c.ping(ctx))
-
-    @cog_ext.cog_slash(name='set', description="Change bot settings")
-    async def set(self, ctx, setting, *, value):
-        await ctx.respond()
-        await ctx.send(embed=await self.c.set(ctx, setting, value))
-
-    @cog_ext.cog_slash(name='get', description="Get bot settings")
-    async def get(self, ctx, setting):
-        await ctx.respond()
-        await ctx.send(embed=await self.c.get(ctx, setting))
-
-    @cog_ext.cog_slash(name='error', description="Cause error")
-    async def error(self, ctx):
-        await ctx.respond()
-        await self.c.error()
+        await ctx.send(await self.c.ping(ctx))
 
     async def on_message(self, message):
         print(message.content)
--- models.py
+++ models.py
@@ -1,11 +1,4 @@
 from peewee import *
-try:
-   import cPickle as pickle
-except ModuleNotFoundError:
-   import pickle
-import settings as st
-import time
-
 
 db = SqliteDatabase('db.sqlite3')
 
@@ -18,31 +11,10 @@ class Base(Model):
 class User(Base):
     user_id = IntegerField(unique=True)
 
-    @staticmethod
-    def get_from_object(author):
-        user = User.get_or_none(user_id=author.id)
-        if user is None:
-            user = User(user_id=author.id)
-            user.save()
-        return user
-
 
 class Role(Base):
     role_id = IntegerField(unique=True)
     name = TextField()
-    position = IntegerField()
-    permissions = IntegerField()
-
-    @staticmethod
-    def get_from_object(role):
-        obj = Role.get_or_none(role_id=role.id)
-        if obj is None:
-            obj = Role(role_id=role.id)
-        obj.name = role.name
-        obj.position = role.position
-        obj.permissions = role.permissions.value
-        obj.save()
-        return obj
 
 
 class Guild(Base):
@@ -51,54 +23,19 @@ class Guild(Base):
     settings = BlobField(null=True)
     roles = ManyToManyField(Role)
 
-    @staticmethod
-    def get_from_object(guild):
-        server = Guild.get_or_none(guild_id=guild.id)
-        if server is None:
-            server = Guild(guild_id=guild.id)
-        server.name = guild.name
-        try:
-            settings = pickle.loads(server.settings)
-        except:
-            settings = {}
-        settings |= st.base_settings
-        server.save()
-        server.roles.clear()
-        for role in guild.roles:
-            server.roles.add(Role.get_from_object(role))
-        server.save()
-        return server, settings
-
-    def save_settings(self, settings):
-        settings |= st.base_settings
-        self.settings = pickle.dumps(settings)
-        self.save()
+    def get_from_object(self, obj):
+        return None
 
 
 class UserInGuild(Base):
     user = ForeignKeyField(User)
-    guild = ForeignKeyField(Guild)
+    guild = ForeignKeyField(User)
     settings = BlobField(null=True)
     roles = BlobField(null=True)
     guild_username = BlobField(null=True)
 
-    @staticmethod
-    def get_from_object(user, guild):
-        obj = UserInGuild.get_or_none(user=user, guild=guild)
-        if obj is None:
-            obj = UserInGuild(user=user, guild=guild)
-        try:
-            settings = pickle.loads(obj.settings)
-        except:
-            settings = {}
-        settings |= st.base_user_settings
-        obj.save()
-        return obj, settings
-
-    def save_settings(self, settings):
-        settings |= st.base_user_settings
-        self.settings = pickle.dumps(settings)
-        self.save()
+    def get_from_object(self, obj):
+        return None
 
 
 class Message(Base):
@@ -106,18 +43,8 @@ class Message(Base):
     user = ForeignKeyField(UserInGuild)
     message_id = IntegerField()
     text = TextField(null=True)
-    timestamp = IntegerField(default=time.time)
+    timestamp = IntegerField()
     latest = BooleanField(True)
-
-    @staticmethod
-    def get_from_object(ctx, user):
-        obj = Message.get_or_none(user=user, message_id=ctx.id)
-        if obj is None:
-            obj = UserInGuild(user=user, message_id=ctx.id)
-        obj.text = ctx.content
-        obj.message_id = ctx.id
-        obj.save()
-        return obj
 
 
 db.connect()
